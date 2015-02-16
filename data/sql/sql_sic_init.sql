585c96a377f2e79a1b0362708214e90767a305cc
formaldehid <marko.sabec@it-portal.si>
2015-02-16 22:34:10 +0100

    c

--- data/sql/sql_sic_init.sql
+++ data/sql/sql_sic_init.sql
@@ -2,4 +2,34 @@ ALTER TABLE `user` ADD `email` VARCHAR(64) NOT NULL;
 ALTER TABLE `user` ADD `notes` VARCHAR(256) NOT NULL;
 
 
+CREATE TABLE `publication` (
+`id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `cobiss` int(11) NOT NULL,
+  `issn` int(11) NOT NULL,
+  `original_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+ALTER TABLE `publication`
+ ADD PRIMARY KEY (`id`);
+
+
+CREATE TABLE `publication_author` (
+`id` int(11) NOT NULL,
+  `publication_id` int(11) NOT NULL,
+  `author` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `publication_author`
+ ADD PRIMARY KEY (`id`);
+
+
+ CREATE TABLE `publication_title` (
+`id` int(11) NOT NULL,
+  `publication_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `publication_title`
+ ADD PRIMARY KEY (`id`);
