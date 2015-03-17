977a1d7a0337a4c50aaa34080e0ddc3cdb86e720
zic2 <matic.vrscaj@gmail.com>
2015-03-17 23:28:49 +0100

    Update

--- module/Sic/Admin/src/Modules/Pub/PubList.js
+++ module/Sic/Admin/src/Modules/Pub/PubList.js
@@ -1,6 +1,7 @@
 var F = function(args) {
 
     var tabPage = args.helpers.createTabPage({name:"List"});
+
     var dataTable = new sic.widget.sicDataTable({
         parent:tabPage.content.selector,
         primaryKey: ['pub_id'],
@@ -13,5 +14,29 @@ var F = function(args) {
             tabPage:tabPage
         }
     });
+
+    /*
+
+    var dataTable = new sic.widget.sicDataTable({
+        parent:tabPage.content.selector,
+        canDelete: false,
+        primaryKey: ['pub_id'],
+        entityTitle: "Pub %pub_id% - %title%",
+        dataSource: new sic.widget.sicDataTableDataSource({
+            moduleName:"Test/TestDT"
+        }),
+        actions: {
+            createPub: {
+                label: 'Create Pub',
+                type: 'button',
+                onClick: function(args) {
+                    sic.dump(args.action, 0);
+                    //alert('Create Pub');
+                }
+            }
+        }
+    });
+
+    */
 
 };
