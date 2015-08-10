0249580d79c76d15b89b3d9aedc112f61c092da8
zic2 <matic.vrscaj@gmail.com>
2015-08-10 18:59:36 +0200

    Update

---
+++ data/sql/clear_db.sql
@@ -0,0 +1,25 @@
+
+TRUNCATE TABLE `publication`;
+TRUNCATE TABLE `publication_addidno`;
+TRUNCATE TABLE `publication_addtitle`;
+TRUNCATE TABLE `publication_creator`;
+TRUNCATE TABLE `publication_doubles_selected`;
+TRUNCATE TABLE `publication_edition`;
+TRUNCATE TABLE `publication_idno`;
+TRUNCATE TABLE `publication_issue`;
+TRUNCATE TABLE `publication_note`;
+TRUNCATE TABLE `publication_online`;
+TRUNCATE TABLE `publication_page`;
+TRUNCATE TABLE `publication_place`;
+TRUNCATE TABLE `publication_project_link`;
+TRUNCATE TABLE `publication_publisher`;
+TRUNCATE TABLE `publication_source`;
+TRUNCATE TABLE `publication_strng`;
+TRUNCATE TABLE `publication_title`;
+TRUNCATE TABLE `publication_volume`;
+TRUNCATE TABLE `publication_year`;
+TRUNCATE TABLE `quote`;
+TRUNCATE TABLE `session`;
+TRUNCATE TABLE `project`;
+TRUNCATE TABLE `project_line`;
+TRUNCATE TABLE `project_line_selected`;
